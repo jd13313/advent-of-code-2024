28f2f1c74dcf4524bf00b359f350e37dee6ec188
Jeremy Duenas <duedev@proton.me>
2024-12-01 23:02:21 -0500

    Added link to challenges

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,28 @@ export const executeChallenge = () => {
     outputField.value = outputValue;
 }
 
+const challenges = [
+    {
+        label: 'Day 1, Part 1',
+        id: '1-1',
+        url: 'https://adventofcode.com/2024/day/1',
+    },
+    {
+        label: 'Day 1, Part 2',
+        id: '1-2',
+        url: 'https://adventofcode.com/2024/day/1',
+    },
+]
+
+const challengeOptions = challenges.map((challenge) => {
+    return `<option value="${challenge.id}">${challenge.label}</option>`;
+})
+
+const updateChallengeLink = (event: Event) => {
+    const selectedChallengeId = (event.target as HTMLSelectElement)?.value ?? '1-1';
+    const selectedChallenge = challenges.find((challenge) => challenge.id === selectedChallengeId);
+    document.querySelector<HTMLAnchorElement>('#view-challenge')?.setAttribute('href', selectedChallenge?.url ?? '');
+}
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="wrapper">
@@ -34,10 +56,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
             
             <div class="dropdown-wrapper">
                 <h2>Select Challenge: </h2>
-                <select id="day-select">
-                    <option value="1-1">Day 1, Part 1</option>
-                    <option value="1-2">Day 1, Part 2</option>
+                <select id="challenge-select">
+                    ${challengeOptions}
                 </select>
+                <a href="${challenges[0]?.url}" target="_blank" id="view-challenge">View Challenge</a>
             </div>
         </div>
         <div id="input-wrapper">
@@ -54,3 +76,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `;
 
 document.querySelector<HTMLButtonElement>('#submit')?.addEventListener('click', executeChallenge);
+document.querySelector<HTMLSelectElement>('#challenge-select')?.addEventListener('change', updateChallengeLink);
